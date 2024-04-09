e1bc2f20c25e6b90beede860a00565eccaae8036
Mateus Melo <mateusdomelo@gmail.com>
2024-04-08 21:32:44 -0300

    Init project

---
+++ src/test/java/io/github/mateusdomelo/javer/JaverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mateusdomelo.javer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
