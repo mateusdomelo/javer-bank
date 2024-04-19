c49a0b160fea67cc9f1b14f95f281cc02f849ed0
Mateus Melo <mateusdomelo@gmail.com>
2024-04-19 10:54:41 -0300

    Update readme, more unit tests and clean up project

--- src/main/java/io/github/mateusdomelo/javer/rest/RequestErrors.java
+++ src/main/java/io/github/mateusdomelo/javer/rest/RequestErrors.java
@@ -1,12 +1,7 @@
 package io.github.mateusdomelo.javer.rest;
 
-import lombok.Getter;
-
-@Getter
-public class RequestErrors {
-    private final String responseMessage;
-
-    public RequestErrors(String message) {
-        this.responseMessage = message;
+public record RequestErrors(String responseMessage) {
+    public String getResponseMessage() {
+        return responseMessage;
     }
 }
